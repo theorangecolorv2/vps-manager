5e0f9336bf52798b1d7aa1b9dc994a78c38d4551
theorr <theorrv2@gmail.com>
2026-01-07 21:15:01 +0300

    .

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,4 +1,4 @@
 # SQLAlchemy models
-from app.models.models import User, Folder, Server
+from app.models.models import User, Folder, Server, Payment, ExchangeRate
 
-__all__ = ["User", "Folder", "Server"]
+__all__ = ["User", "Folder", "Server", "Payment", "ExchangeRate"]
